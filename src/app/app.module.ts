61957f852d5ae726914aff2f4c0bc6a7b0cca34d
wesleycaldeiradeoliveira <wesleycaldeiradeoliveira>
2024-02-14 14:39:42 -0300

    Utilização de Componentes de outros Módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,17 @@ import { CardComponent } from './card/card.component';
 import { CardRosaComponent } from './card-rosa/card-rosa.component';
 import { CardButtonComponent } from './card-button/card-button.component';
 import { CardRosaButtonComponent } from './card-rosa-button/card-rosa-button.component';
+import { CardsModule } from './cards/cards.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
-    CardRosaComponent,
-    CardButtonComponent,
-    CardRosaButtonComponent
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardsModule,
     
   ],
   providers: [],
--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from '../card/card.component';
+import { CardButtonComponent } from '../card-button/card-button.component';
+import { CardRosaComponent } from '../card-rosa/card-rosa.component';
+import { CardRosaButtonComponent } from '../card-rosa-button/card-rosa-button.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CardComponent,
+    CardButtonComponent,
+    CardRosaComponent,
+    CardRosaButtonComponent,
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+      ],
+      exports:[
+        CardComponent,
+    CardButtonComponent,
+    CardRosaComponent,
+    CardRosaButtonComponent,
+      ]
 })
 export class CardsModule { }
