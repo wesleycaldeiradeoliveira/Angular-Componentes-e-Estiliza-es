1f62cbd50f15c8d66eec539f31aa3b4d45575d5f
wesleycaldeiradeoliveira <wesleycaldeiradeoliveira>
2024-02-15 11:36:14 -0300

    Importar Arquivos de Estilos com @import

--- src/app/card-button-cancel/card-button-cancel.component.ts
+++ src/app/card-button-cancel/card-button-cancel.component.ts
@@ -3,21 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button-cancel',
   templateUrl: './card-button-cancel.component.html',
-  //styleUrl: './card-button-cancel.component.scss'
-  styles: [
-  `
- .card-button-cancel {
-    font-size: 17px;
-    text-align: center;
-    margin-top: 10px;
-    border-radius: 25px;
-    background-color: rgb(13, 105, 105);
-    cursor: pointer;
-    font-weight: bold;
-    padding: 10px;
-}
-  ` 
-  ],
+  styleUrl:'./card-button-cancel.component.scss',
+  
 })
 export class CardButtonCancelComponent {
 
