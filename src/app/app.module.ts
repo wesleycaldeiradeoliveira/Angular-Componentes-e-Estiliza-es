46139bf8631b7a6188c75db0c607c5d4270dfcbb
wesleycaldeiradeoliveira <wesleycaldeiradeoliveira>
2024-02-15 10:34:54 -0300

    Estilos CSS Dentro do Componente (prop styles)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import { CardRosaComponent } from './card-rosa/card-rosa.component';
 import { CardButtonComponent } from './card-button/card-button.component';
 import { CardRosaButtonComponent } from './card-rosa-button/card-rosa-button.component';
 import { CardsModule } from './cards/cards.module';
+import { CardButtonCancelComponent } from './card-button-cancel/card-button-cancel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    
    
   ],
   imports: [
--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -4,6 +4,7 @@ import { CardComponent } from '../card/card.component';
 import { CardButtonComponent } from '../card-button/card-button.component';
 import { CardRosaComponent } from '../card-rosa/card-rosa.component';
 import { CardRosaButtonComponent } from '../card-rosa-button/card-rosa-button.component';
+import { CardButtonCancelComponent } from '../card-button-cancel/card-button-cancel.component';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { CardRosaButtonComponent } from '../card-rosa-button/card-rosa-button.co
     CardButtonComponent,
     CardRosaComponent,
     CardRosaButtonComponent,
+    CardButtonCancelComponent,
   ],
   imports: [
     CommonModule,
